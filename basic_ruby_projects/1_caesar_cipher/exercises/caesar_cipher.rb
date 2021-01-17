c78c7a18303499fe8196279345fe0f68de234815
Jonathan Hall <jonathan.edward.hall@gmail.com>
2021-01-17 13:38:34 -0500

    Rename folders, pass all basic ruby exercise tests, and solve the caesar cipher basic ruby project.

---
+++ basic_ruby_projects/1_caesar_cipher/exercises/caesar_cipher.rb
@@ -0,0 +1,26 @@
+def caesar_cipher(message, shift_factor)
+  chars = message.chars # Make an array of all characters.
+
+  chars.map! do |c|
+    ascii_code = c.ord.to_i # Turn character into ascii.
+
+    # Only shift the letters a-z and A-Z.
+    if ascii_code.between? 65, 90
+      ascii_code = (ascii_code + shift_factor - 64) % 26 + 64
+    elsif ascii_code.between? 97, 122
+      ascii_code = (ascii_code + shift_factor - 96) % 26 + 96
+    end
+
+    ascii_code.chr # Turn ascii back into character.
+  end
+
+  letters.join # Mush the array back into a single string.
+end
+
+puts "Enter a message to run through the caesar cypher: "
+input = gets.chomp
+
+puts "Enter the shift factor (1 - 25): "
+shift = gets.chomp.to_i
+
+puts caesar_cipher(input, shift)
